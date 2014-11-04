9304b4eba07df0280a91821132979677e3583c1e
dlernatovich <dlernatovich@geeksforless.net>
2014-11-04 20:23:09 +0200

    Minor changes

--- app/src/main/java/artlite/com/androidsupportlibraryproject/ui/activities/ChartActivity.java
+++ app/src/main/java/artlite/com/androidsupportlibraryproject/ui/activities/ChartActivity.java
@@ -4,12 +4,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import com.artlite.library.compilation.ui.activities.BaseLibraryActivity;
+import com.github.mikephil.charting.charts.BarLineChartBase;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import artlite.com.androidsupportlibraryproject.R;
 
@@ -17,40 +19,50 @@ public class ChartActivity extends BaseLibraryActivity {
 
     private LineChart mChart;
 
+    private static final String[] X_VALUES = new String[]{
+            "Monday", "Tuesday", "Wednesday", "Friday"
+    };
+
+    private static final float[] Y_VALUES = new float[]{
+            12.7f, 15.5f, 22.5f, -6.5f
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
         setHeaderToUpperCase(getString(R.string.title_activity_chart));
         mChart = (LineChart) findViewById(R.id.lineChart1);
-        setData(10, 10);
 
-//        mChart.setDrawUnitsInChart(true);
-//        mChart.setStartAtZero(false);
-//        mChart.setDrawYValues(false);
-//        mChart.setDrawBorder(true);
-//        mChart.setBorderPositions(new BarLineChartBase.BorderPosition[]{
-//                BarLineChartBase.BorderPosition.BOTTOM
-//        });
+        initializeData(X_VALUES, Y_VALUES);
+        initializeGraph(mChart);
+    }
+
+    private void initializeGraph(LineChart mChart) {
+        mChart.setDrawUnitsInChart(true);
+        mChart.setStartAtZero(false);
+        mChart.setDrawYValues(false);
+        mChart.setDrawBorder(true);
+        mChart.setDrawXLabels(true);
+        mChart.setBorderPositions(new BarLineChartBase.BorderPosition[]{
+                BarLineChartBase.BorderPosition.BOTTOM
+        });
         mChart.setDescription("");
-//        mChart.setNoDataTextDescription("You need to provide data for the chart.");
         mChart.setHighlightEnabled(true);
         mChart.setTouchEnabled(true);
         mChart.setDragEnabled(true);
         mChart.setScaleEnabled(true);
         mChart.setPinchZoom(true);
-
+        mChart.getLegend().setTextColor(Color.WHITE);
+        mChart.getXLabels().setTextColor(Color.WHITE);
+        mChart.getYLabels().setTextColor(Color.WHITE);
     }
 
-    private void setData(int count, float range) {
-        ArrayList<String> xVals = new ArrayList<String>();
-        for (int i = 0; i < count; i++) {
-            xVals.add((i) + "");
-        }
+    private void initializeData(String[] xValues, float[] yValues) {
+        ArrayList<String> xVals = new ArrayList<String>(Arrays.asList(xValues));
         ArrayList<Entry> yVals = new ArrayList<Entry>();
-        for (int i = 0; i < count; i++) {
-            float mult = (range + 1);
-            float val = (float) (Math.random() * mult) + 3;// + (float)
+        for (int i = 0; i < yValues.length; i++) {
+            float val = yValues[i];
             yVals.add(new Entry(val, i));
         }
         LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
@@ -63,19 +75,8 @@ public class ChartActivity extends BaseLibraryActivity {
         ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
         dataSets.add(set1); // add the datasets
         LineData data = new LineData(xVals, dataSets);
-//        LimitLine ll1 = new LimitLine(130f);
-//        ll1.setLineWidth(4f);
-//        ll1.enableDashedLine(10f, 10f, 0f);
-//        ll1.setDrawValue(true);
-//        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT);
-//        LimitLine ll2 = new LimitLine(-30f);
-//        ll2.setLineWidth(4f);
-//        ll2.enableDashedLine(10f, 10f, 0f);
-//        ll2.setDrawValue(true);
-//        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT);
-//        data.addLimitLine(ll1);
-//        data.addLimitLine(ll2);
         mChart.setData(data);
+        mChart.setValueTextColor(Color.WHITE);
         mChart.fitScreen();
     }
 
